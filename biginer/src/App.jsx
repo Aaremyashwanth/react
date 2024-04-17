0ee96898706c6c5ae90d9fa11e588f0acdfb29b9
Yashwanth Aarem <aaremyashwanth@gmail.com>
2024-04-17 10:30:49 +0530

    react created

---
+++ biginer/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <center>
+      <div>
+       <h1>YASHWANTH</h1>
+       <h2>cse-iot</h2>
+      </div>
+     
+    </center>
+  )
+}
+
+export default App
